dff795da93220f44a1d65dd27c1c9ae3dc17cc7c
Vish Vishvanath <vishvish@users.noreply.github.com>
2018-01-30 17:33:25 +0000

    Add composer directives to install graphql plugin Installs composer into container, installs graphql plugin and dependencies.

--- 4/rootfs/app-entrypoint.sh
+++ 4/rootfs/app-entrypoint.sh
@@ -5,6 +5,13 @@
 
 print_welcome_page
 
+echo >&2 "Setting up autoload"
+    if ! grep -Fxq "require_once __DIR__.\"/../../../wp-content/vendor/autoload.php\";" /opt/bitnami/wordpress/wp-content/plugins/graphql-wp/index.php
+    then
+      sed -i '/namespace Mohiohio\\GraphQLWP;/a require_once __DIR__."/../../../wp-content/vendor/autoload.php";' /opt/bitnami/wordpress/wp-content/plugins/graphql-wp/index.php
+    fi
+    sed -i '/<?php/a require __DIR__ . "/wp-content/vendor/autoload.php";' /opt/bitnami/wordpress/wp-config.php
+
 if [[ "$1" == "nami" && "$2" == "start" ]] || [[ "$1" == "/init.sh" ]]; then
   . /init.sh
   nami_initialize apache php mysql-client wordpress
